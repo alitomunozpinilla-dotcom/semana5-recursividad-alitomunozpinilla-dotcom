9a6ae56c557e1a736d545258bf563c5d80d8b6af
alitomunozpinilla-dotcom <alitomunozpinilla@gmail.com>
2026-04-06 17:54:08 -0400

    Ejercicios5

--- Semana5/ejercicio5.py
+++ Semana5/ejercicio5.py
@@ -4,8 +4,14 @@ Calcular la potencia de una base elevada a un exponente entero positivo.
 """
 
 def potencia_ciclo(base, exponente):
-    pass
-
+    Resultado = 1
+    for i in range(exponente):
+        Resultado *= base
+    return Resultado
+print(potencia_ciclo(2, 5))
 
 def potencia_recursiva(base, exponente):
-    pass
+    if exponente == 0:
+        return 1
+    return base * potencia_recursiva(base, exponente - 1)
+print(potencia_recursiva(2, 5))
